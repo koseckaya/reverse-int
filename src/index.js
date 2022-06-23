a55b838da275bd9cf53a8bc6859884fc52df9f8a
interika <interika11@gmail.com>
2022-06-23 17:11:14 +0300

    final function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+
+    let strNum = String(Math.abs(n))
+    let arrStr = strNum.split("")
+    let reverseArr = arrStr.reverse()
+    let arrToStr = reverseArr.join('')
+    let strToNum = +arrToStr
+
+    return strToNum
 }
+
